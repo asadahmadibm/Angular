98b74dfcc77ae7ae376760900952c38328f1b38b
Asad Ahmadi <asad.ahmadi.bm@gmail.com>
2022-11-07 11:56:52 +0330

    fix hieght grid

--- src/app/commonComponent/admin-grid/admin-grid.component.ts
+++ src/app/commonComponent/admin-grid/admin-grid.component.ts
@@ -1,29 +1,20 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { ColDef } from 'ag-grid-community';
+import { Component, Input, OnInit } from "@angular/core";
+import { ColDef } from "ag-grid-community";
 
 @Component({
-  selector: 'app-admin-grid',
-  templateUrl: './admin-grid.component.html',
-  styleUrls: ['./admin-grid.component.css']
+  selector: "app-admin-grid",
+  templateUrl: "./admin-grid.component.html",
+  styleUrls: ["./admin-grid.component.css"],
 })
-
 export class AdminGridComponent implements OnInit {
-
-  @Input() datagrid:any;
-  @Input() columnsgrid:any;
-  private gridApi:any;
-
-
-
-
-          onGridReady(params:any) {
-            params.api.sizeColumnsToFit();
-            this.gridApi = params.api;
-
-          }
-  constructor() { }
-
-  ngOnInit(): void {
+  @Input() datagrid: any;
+  @Input() columnsgrid: any;
+  private gridApi: any;
+  onGridReady(params: any) {
+    params.api.sizeColumnsToFit();
+    this.gridApi = params.api;
   }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
